e1bab833d6f26b700f735d0be30f3d4b050c7224
Martti <aaro.yliayho@gmail.com>
2021-10-17 13:07:34 +0300

    Puhelinluettelo 3.5 - 3.8

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const express = require('express')
-const app = express()
+const { json } = require('express');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// pari tuntia debugattiin ja lopulta olikin express.json'()' 
+// mikä puuttui...........
+// tekisi mieli lyödä jotain...
+// (se jättää localhostin lataamaan ikuisesti)
+app.use(express.json());
+// Morgan mutta POST tyyppiset pyynnöt skipataan
+app.use(morgan('tiny', {
+    skip: (req, res) => { return req.method === 'POST' }
+}));
+// tämä logaa aiemmin skipatut POST tyyppiset pyynnöt custom
+// muodossa tehtävän mukaan
+app.use(morgan(':method :url :status :res[content-length] - response-time ms :body', {
+    skip: (req, res) => { return req.method !== 'POST'}
+}));
+
+morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+
+
+const generateId = () => {
+    const max = 1000000000
+    newId = Math.floor(Math.random() * max)
+    return newId
+}
 
 let persons = [
     {
@@ -24,26 +50,31 @@ let persons = [
     },
 ]
 
+// kaikki ihmiset
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+// info sivu
 app.get('/info', (req, res) => {
     res.send(
         `<p>Phonebook has info of ${persons.length}</p>${Date()}`
     )
 })
 
+// yksittäinen ihminen
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
-    const person = persons.find(person => person.id === id)
+    const person = persons.find(person => person.id === id);
     if (person) {
-        res.json(person)
+        res.json(person);
     } else {
-        res.status(404).end()
+        return res.status(404).end();
     }
+})
 
+// poista ihminen
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
 
@@ -55,13 +86,42 @@ app.delete('/api/persons/:id', (req, res) => {
         res.status(204).end()
         console.log('Person deleted')
     } else {
-        res.status(404).end()
+        return res.status(404).end()
     }
 })
 
-    
+// lisää ihminen (toivottavasti)
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    console.log('Body:', body)
+
+    // jos ei ole nimeä tai numeroa
+    if (!body.name || !body.number) {
+        // virhekoodin palautus
+        return res.status(404).json({'error': 'name or number missing'})
+    } 
+
+    const nameExists = (name) => {
+        return persons.find(person => person.name === name)
+    }
+
+    if (nameExists(body.name)) {
+        return res.status(404).json({'error': 'name must be unique'})
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    console.log('New persons:', persons)
+
+    res.status(200).json(person)
 })
-PORT = 3001
+    
+PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
