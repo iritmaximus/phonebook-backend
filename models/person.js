747c7d01c55e6b535ba4cdbb81a046cb08d057cf
iritmaximus <aaro.yliayho@gmail.com>
2022-02-22 20:29:57 +0200

    3.19 done

---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url)
+    .then(res => {
+        console.log("connected to mongodb:", url);
+    })
+    .catch((error) => {
+        console.log("failed to connect:", error.message);
+    })
+
+const personSchema = mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+});
+
+personSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+
+module.exports = mongoose.model("Person", personSchema);
