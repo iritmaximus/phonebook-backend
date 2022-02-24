ea4c8c0189c4722d9c73ad23090dbb84e89a1358
iritmaximus <aaro.yliayho@gmail.com>
2022-02-24 21:09:07 +0200

    number bug fix

--- models/person.js
+++ models/person.js
@@ -18,7 +18,7 @@ const personSchema = mongoose.Schema({
         required: true
     },
     number: {
-        type: Number,
+        type: String,
         required: true,
         validate: { 
           validator: function(testNumber) {
