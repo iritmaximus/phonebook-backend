a525a4fcf25e5cd40435e1242ce1c9ae90ab8106
iritmaximus <aaro.yliayho@gmail.com>
2022-03-03 17:15:38 +0200

    added linting with eslint

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const Person = require("./models/person.js");
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 
-// pari tuntia debugattiin ja lopulta olikin express.json'()' 
+// pari tuntia debugattiin ja lopulta olikin express.json'()'
 // mikä puuttui...........
 // tekisi mieli lyödä jotain...
 // (se jättää localhostin lataamaan ikuisesti)
 app.use(express.json());
 // Morgan mutta POST tyyppiset pyynnöt skipataan
-app.use(morgan('tiny', {
-    skip: (req, res) => { return req.method === 'POST' }
+app.use(morgan("tiny", {
+  skip: (req, res) => { return req.method === "POST"; }
 }));
 // tämä logaa aiemmin skipatut POST tyyppiset pyynnöt custom
 // muodossa tehtävän mukaan
-app.use(morgan(':method :url :status :res[content-length] - response-time ms :body', {
-    skip: (req, res) => { return req.method !== 'POST'}
+app.use(morgan(":method :url :status :res[content-length] - response-time ms :body", {
+  skip: (req, res) => { return req.method !== "POST";}
 }));
 
 // uusi morgan token joka palauttaa json muodosssa pyynnön (POST) bodyn
-morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+morgan.token("body", (req, res) => { return JSON.stringify(req.body); });
 
 // ottaa käyttöön frontendin backendin kautta
-app.use(express.static('build'));
+app.use(express.static("build"));
 app.use(cors());
 
 
@@ -33,121 +33,120 @@ app.use(cors());
 
 
 // kaikki ihmiset
-app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons);
-    })
-})
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons);
+  });
+});
 
 // info sivu
-app.get('/info', (req, res) => {
-    // vähän ruma ratkaisu mutta se toimii, mur muuttuvat arvot backendissä
-    var i = 0;
-    Person.find({}).then(persons => {
-        persons.forEach(i++);
-        res.send(
-            `<p>Phonebook has info of ${i}</p>${Date()}`
-        )   
-    })
-    
-})
+app.get("/info", (req, res) => {
+  // vähän ruma ratkaisu mutta se toimii, mur muuttuvat arvot backendissä
+  var i = 0;
+  Person.find({}).then(persons => {
+    persons.forEach(i++);
+    res.send(
+      `<p>Phonebook has info of ${i}</p>${Date()}`
+    );
+  });
+
+});
 
 // yksittäinen ihminen
-app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person);
-            } else {
-                res.status(404).send({ "error": "no person found" });
-            }
-        })
-        .catch(error => next(error));
-})
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).send({ "error": "no person found" });
+      }
+    })
+    .catch(error => next(error));
+});
 
 // poista ihminen
-app.delete('/api/persons/:id', (req, res, next) => {
-    // etsitään ihminen joka halutaan poistaa
-    Person.findByIdAndDelete(req.params.id)
-        .then(person => {
-            if (person) {
-                console.log("deleted:", person);
-            res.json(person);
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(error => next(error));
+app.delete("/api/persons/:id", (req, res, next) => {
+  // etsitään ihminen joka halutaan poistaa
+  Person.findByIdAndDelete(req.params.id)
+    .then(person => {
+      if (person) {
+        console.log("deleted:", person);
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 
-})
+});
 
 app.put("/api/persons/:id", (req, res, next) => {
 
-    Person.findByIdAndUpdate(req.params.id, { number: req.body.number })
-        .then(person => {
-            res.json(person);
-        })
-        .catch(error => next(error));
-})
-
-
-
-app.post('/api/persons', (req, res, next) => {
-    const body = req.body
-    console.log('Body:', body)
-
-
-    // jos ei ole nimeä tai numeroa
-    if (!body.name || !body.number) {
-        return res.status(400).json({'error': 'name or number missing'})
-    } 
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  Person.findByIdAndUpdate(req.params.id, { number: req.body.number })
+    .then(person => {
+      res.json(person);
     })
+    .catch(error => next(error));
+});
 
-    // katsotaan onko olemassa jo samanniminen
-    Person.find({ name: body.name })
-        .then(persons => {
-            console.log("Result:", persons)
-            if (persons.length === 0) {
-                // jos ei ole samannimistä, tallennetaan db:hen
-                person
-                    .save()
-                        .then(savedPerson => {
-                            res.json(savedPerson);
-                        })
-                        .catch(error => next(error));
 
-            } else {
-                return res.status(404).json({'error': 'name must be unique'})
-            }
-        })
-        .catch(error => next(error));
+
+app.post("/api/persons", (req, res, next) => {
+  const body = req.body;
+  console.log("Body:", body);
+
+
+  // jos ei ole nimeä tai numeroa
+  if (!body.name || !body.number) {
+    return res.status(400).json({ "error": "name or number missing" });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  // katsotaan onko olemassa jo samanniminen
+  Person.find({ name: body.name })
+    .then(persons => {
+      console.log("Result:", persons);
+      if (persons.length === 0) {
+        // jos ei ole samannimistä, tallennetaan db:hen
+        person
+          .save()
+          .then(savedPerson => {
+            res.json(savedPerson);
+          })
+          .catch(error => next(error));
+
+      } else {
+        return res.status(404).json({ "error": "name must be unique" });
+      }
+    })
+    .catch(error => next(error));
 });
 
 
 
 // error handler... duh
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message);
+  console.error(error.message);
 
-    if (error.name === "CastError") {
-       return res.status(400).send({ "error": "wrong id format" });
-    } else if (error.name === "ValidationError" && error.path === "number"){
-        return res.status(400).send({ "error": "Number is not correctly formatted" })    
-    } else if (error.name === "ValidationError") {
-        return res.status(400).send({ "error": error.message })
-    }
+  if (error.name === "CastError") {
+    return res.status(400).send({ "error": "wrong id format" });
+  } else if (error.name === "ValidationError" && error.path === "number"){
+    return res.status(400).send({ "error": "Number is not correctly formatted" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ "error": error.message });
+  }
 
-    next(error);
-}
+  next(error);
+};
 
 app.use(errorHandler);
 
-    
 PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
--- models/person.js
+++ models/person.js
@@ -3,38 +3,36 @@ const mongoose = require("mongoose");
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
-    .then(res => {
-        console.log("connected to mongodb:", url);
-    })
-    .catch((error) => {
-        console.log("failed to connect:", error.message);
-    })
+  .then(console.log("connected to mongodb:", url))
+  .catch((error) => {
+    console.log("failed to connect:", error.message);
+  });
 
 
 const personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(testNumber) {
+        return /\d{2,3}-\d{7,}/.test(testNumber);
+      },
+      message: props => `${props.value} is not correctly formatted.`
     },
-    number: {
-        type: String,
-        required: true,
-        validate: { 
-          validator: function(testNumber) {
-            return /\d{2,3}-\d{7,}/.test(testNumber) 
-          },
-          message: props => `${props.value} is not correctly formatted.`
-        },
-    },
+  },
 });
 
 personSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
 });
 
 module.exports = mongoose.model("Person", personSchema);
